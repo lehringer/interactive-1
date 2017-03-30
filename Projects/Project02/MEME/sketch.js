25403257701a455e4b2ac560975f9ff08e97b80c
lehringer <logibear56@MacBook-Pro-6.local>
2017-03-30 13:28:31 -0700

    animate

--- Projects/Project02/MEME/sketch.js
+++ Projects/Project02/MEME/sketch.js
@@ -6,6 +6,15 @@ var song5;
 var song6;
 var song7;
 var song8;
+var tformR1 = document.querySelector("#topred");
+//var tformC1 = document.querySelector("#topcyan");
+//var tformG1 = document.querySelector("#topgelb");
+//var tformM1 = document.querySelector("#topmagenta");
+
+//var tformR2 = document.querySelector("#bottomred");
+//var tformC2 = document.querySelector("#bottomcyan");
+//var tformG2 = document.querySelector("#bottomgelb");
+//var tformM2 = document.querySelector("#bottommagenta");
 
 function setup(){
 	song1 = loadSound("gocarol.mp3");
@@ -16,18 +25,38 @@ function setup(){
 	song6 = loadSound("worth.mp3");
 	song7 = loadSound("CHUNKY.mp3");
 	song8 = loadSound("change.mp3");
+	tformR1.addEventListener("mouseover", setInitialClass, false);
+	    tformR1.addEventListener("transitionend", loopTransition, false);
+	    tformR1.addEventListener("webkitTransitionEnd", loopTransition, false);
+	    tformR1.addEventListener("mozTransitionEnd", loopTransition, false);
+	    tformR1.addEventListener("msTransitionEnd", loopTransition, false);
+	    tformR1.addEventListener("oTransitionEnd", loopTransition, false);
+
 	
 	createCanvas(windowWidth,windowHeight);
 	}
 
-function draw(){
+function setInitialClass(e){
+	tformR1.className = "stateTwo";
+	tformC1.className = "stateTwo";
+	tformG1.className = "stateTwo";
+	tformM1.className = "stateTwo";
+}
 
-if (mousePressed) {
+function loopTransition(e){
+	if(e.propertyName == "opacity"){
+		if(tformR1.className == "stateTwo"){
+			tformR1.className = "stateOne";
+		} else{
+			tformR1.className = "stateTwo";
+		}
+	}
+}
+
+function mousePressed(){
 
 	var songArray = [song1, song2, song3, song4, song5, song6, song7, song8];
 	var randomsong = random(songArray);
     randomsong.play();
-    randomsong.loop() = false;
 
-}
 }
